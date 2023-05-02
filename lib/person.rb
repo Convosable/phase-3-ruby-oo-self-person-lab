084d3f62f836f9fa7ccc6275ef7d3ab2fe30a969
Connor Vosberg <connorjv@gmail.com>
2023-05-01 19:07:48 -0700

    commit tree ec55385c9546a57a223a977eb1e629872ca00087

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,91 @@
-# your code goes here
+require "pry"
+
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize (name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(happiness_level)
+        if happiness_level > 10
+            @happiness = 10
+        elsif happiness_level < 0
+            @happiness = 0
+        else
+            @happiness = happiness_level
+        end
+    end
+
+    def hygiene=(hygiene_level)
+        if hygiene_level > 10
+            @hygiene = 10
+        elsif hygiene_level < 0
+            @hygiene = 0
+        else
+            @hygiene = hygiene_level
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = @hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == 'politics'
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == 'weather'
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+
+end
+
+# p1 = Person.new('Jessica')
+
+# binding.pry
